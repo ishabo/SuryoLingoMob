b186aee46cd07d10ee66d7e2dde5ae7d07e12711
Issa Shabo <issashabo@gmail.com>
2019-10-07 18:53:57 +0100

    fix signin

--- src/services/profile/api/index.ts
+++ src/services/profile/api/index.ts
@@ -1,15 +1,17 @@
-import { create } from '@sl/services/api';
-import { IProfilePayload } from '../';
+import { create } from "@sl/services/api";
+import { IProfilePayload } from "../";
 
-import { injectDeviceInfo } from '@sl/helpers';
+import { injectDeviceInfo } from "@sl/helpers";
 
 export const createProfile = async (payload: IProfilePayload = {}) => {
   // Creating Profile is the only API call that's done with a default token
-  return create().post('/users', await injectDeviceInfo(payload));
+  return create().post("/users", await injectDeviceInfo(payload));
 };
 
-export const updateProfile = (id: string) => async (payload: IProfilePayload = {}) => {
+export const updateProfile = (id: string) => async (
+  payload: IProfilePayload = {}
+) => {
   return create().put(`/users/${id}`, await injectDeviceInfo(payload));
 };
 
-export const getUser = () => create().get('/users');
+export const getUser = () => create().get("/users");
--- src/services/signon/sagas/index.ts
+++ src/services/signon/sagas/index.ts
@@ -1,49 +1,61 @@
-import { call, put, select } from 'redux-saga/effects';
-import { delay } from 'redux-saga';
-import * as signon from '@sl/services/signon';
-import * as profile from '@sl/services/profile';
-import * as progress from '@sl/services/progress';
-import * as skills from '@sl/services/skills';
-import * as courses from '@sl/services/courses';
-import { IInitialState } from '@sl/services/reducers';
-import { isEmpty } from 'lodash';
-import { validateSigon } from '../validation';
-import { setLoadingOn, setLoadingOff, setFailureMessage, setSuccessMessage } from '@sl/services/api/actions';
-import { ISagasFunctions } from '@sl/services/sagas';
-import { getActiveCourse } from '@sl/services/selectors';
-import { isApiResponse, resetToSkills, navToCourses } from '@sl/helpers';
-import RNRestart from 'react-native-restart';
-import { deleteAccessToken } from '@sl/services/api/access';
-import { LoginManager, AccessToken } from 'react-native-fbsdk';
-import { NavigationActions } from 'react-navigation';
-import { Analytics } from '@sl/config/firebase';
-import { logError } from '@sl/helpers';
+import { call, put, select } from "redux-saga/effects";
+import { delay } from "redux-saga";
+import * as signon from "@sl/services/signon";
+import * as profile from "@sl/services/profile";
+import * as progress from "@sl/services/progress";
+import * as skills from "@sl/services/skills";
+import * as courses from "@sl/services/courses";
+import { IInitialState } from "@sl/services/reducers";
+import { isEmpty } from "lodash";
+import { validateSigon } from "../validation";
+import {
+  setLoadingOn,
+  setLoadingOff,
+  setFailureMessage,
+  setSuccessMessage
+} from "@sl/services/api/actions";
+import { ISagasFunctions } from "@sl/services/sagas";
+import { getActiveCourse } from "@sl/services/selectors";
+import { isApiResponse, resetToSkills, navToCourses } from "@sl/helpers";
+import RNRestart from "react-native-restart";
+import { deleteAccessToken } from "@sl/services/api/access";
+import { LoginManager, AccessToken } from "react-native-fbsdk";
+import { NavigationActions } from "react-navigation";
+import { Analytics } from "@sl/config/firebase";
+import { logError } from "@sl/helpers";
 
 function* captureBadRequest(response, errors): IterableIterator<any> {
   // logError(JSON.stringify(response));
 
   if (response.status === 400) {
-    if (typeof response.data === 'object') {
-      if (Array.isArray(response.data['email']) && response.data['email'].indexOf('already exists') !== -1) {
-        errors['email'] = 'emailAlreadyExists';
+    if (typeof response.data === "object") {
+      if (
+        Array.isArray(response.data["email"]) &&
+        response.data["email"].indexOf("already exists") !== -1
+      ) {
+        errors["email"] = "emailAlreadyExists";
       }
       if (
-        Array.isArray(response.data['facebook_id']) &&
-        response.data['facebook_id'].indexOf('already exists') !== -1
+        Array.isArray(response.data["facebook_id"]) &&
+        response.data["facebook_id"].indexOf("already exists") !== -1
       ) {
-        errors['facebook'] = 'facebookAlreadyConnected';
+        errors["facebook"] = "facebookAlreadyConnected";
       }
     }
     yield put(signon.actions.setErrors(errors));
   }
 }
 
-export function* submitSignon(action: signon.ISignonFormAction): IterableIterator<any> {
+export function* submitSignon(
+  action: signon.ISignonFormAction
+): IterableIterator<any> {
   yield put(setLoadingOn());
-  const fields = { ...(yield select((state: IInitialState) => state.signon.item)) };
+  const fields = {
+    ...(yield select((state: IInitialState) => state.signon.item))
+  };
 
-  if (action.signon === 'signin') {
-    delete fields['name'];
+  if (action.signon === "signin") {
+    delete fields["name"];
   }
 
   const errors: signon.ISignonFormErrors = validateSigon(fields);
@@ -53,11 +65,11 @@ export function* submitSignon(action: signon.ISignonFormAction): IterableIterato
     let profileData = yield select((state: IInitialState) => state.profile);
 
     try {
-      if (action.signon === 'signin') {
+      if (action.signon === "signin") {
         profileData = yield call(signon.api.signin, fields);
         yield put(profile.actions.saveProfileAndAccessToken(profileData));
       } else {
-        profileData = yield call(profile.api.updateProfile(profileData.id), fields);
+        profileData = yield call(profile.api.createProfile, fields);
         yield put(profile.actions.saveProfileAndAccessToken(profileData));
       }
 
@@ -86,28 +98,50 @@ export function* submitSignon(action: signon.ISignonFormAction): IterableIterato
   yield put(setLoadingOff());
 }
 
-export function* connectViaFacebook(actions: signon.ISignonFormAction): IterableIterator<any> {
-  Analytics.logEvent('connect_via_facebook', { SignonType: actions.signon, Started: true });
+export function* connectViaFacebook(
+  actions: signon.ISignonFormAction
+): IterableIterator<any> {
+  Analytics.logEvent("connect_via_facebook", {
+    SignonType: actions.signon,
+    Started: true
+  });
 
   try {
-    const result = yield call(LoginManager.logInWithReadPermissions, ['public_profile', 'email']);
+    const result = yield call(LoginManager.logInWithReadPermissions, [
+      "public_profile",
+      "email"
+    ]);
     if (result.isCancelled) {
-      Analytics.logEvent('connect_via_facebook', { SignonType: actions.signon, Cancelled: true });
+      Analytics.logEvent("connect_via_facebook", {
+        SignonType: actions.signon,
+        Cancelled: true
+      });
     } else {
       const { accessToken } = yield call(AccessToken.getCurrentAccessToken);
-      Analytics.logEvent('connect_via_facebook', { SignonType: actions.signon, Successful: true });
+      Analytics.logEvent("connect_via_facebook", {
+        SignonType: actions.signon,
+        Successful: true
+      });
 
-      const facebookProfileData = yield call(signon.api.getFacebookProfile, accessToken);
+      const facebookProfileData = yield call(
+        signon.api.getFacebookProfile,
+        accessToken
+      );
 
       const payload = {
         password: accessToken,
         viaFacebook: true
       };
 
-      const currentProfile = yield select((state: IInitialState) => state.profile);
+      const currentProfile = yield select(
+        (state: IInitialState) => state.profile
+      );
 
-      if (actions.signon === 'connect') {
-        const profileData = yield call(profile.api.updateProfile(currentProfile.id), payload);
+      if (actions.signon === "connect") {
+        const profileData = yield call(
+          profile.api.updateProfile(currentProfile.id),
+          payload
+        );
         yield put(profile.actions.saveProfileAndAccessToken(profileData));
       } else {
         const { email, name } = facebookProfileData;
@@ -124,21 +158,25 @@ export function* connectViaFacebook(actions: signon.ISignonFormAction): Iterable
       yield call(captureBadRequest, error.response, errors);
     } else {
       logError(JSON.stringify(error));
-      yield put(signon.actions.setErrors({ facebook: 'failedToLoginViaFacebook' }));
+      yield put(
+        signon.actions.setErrors({ facebook: "failedToLoginViaFacebook" })
+      );
     }
   }
   yield put(setLoadingOff());
 }
 
-export function* recoverPassword(action: signon.ISignonFormAction): IterableIterator<any> {
+export function* recoverPassword(
+  action: signon.ISignonFormAction
+): IterableIterator<any> {
   yield put(setLoadingOn());
   try {
     yield call(signon.api.recoverPassword, action.email);
-    yield put(setSuccessMessage('passwordRecoverySuccess', true));
+    yield put(setSuccessMessage("passwordRecoverySuccess", true));
   } catch (error) {
     if (isApiResponse(error)) {
       if (error.response.status === 422) {
-        yield put(setFailureMessage('passwordRecoveryFailure', true));
+        yield put(setFailureMessage("passwordRecoveryFailure", true));
       }
     }
   }
@@ -147,7 +185,7 @@ export function* recoverPassword(action: signon.ISignonFormAction): IterableIter
 }
 
 export function* signout(): IterableIterator<any> {
-  yield put(NavigationActions.navigate({ routeName: 'DrawerClose' }));
+  yield put(NavigationActions.navigate({ routeName: "DrawerClose" }));
   yield put(setLoadingOn());
   yield delay(500);
   yield put(profile.actions.resetProfile());
@@ -155,7 +193,7 @@ export function* signout(): IterableIterator<any> {
   yield put(skills.actions.resetSkills());
   yield put(courses.actions.resetCourses());
   yield call(deleteAccessToken);
-  Analytics.logEvent('signout_clicked', {});
+  Analytics.logEvent("signout_clicked", {});
   yield delay(500);
   yield put(setLoadingOff());
 
@@ -166,5 +204,8 @@ export const functions = (): ISagasFunctions[] => [
   { action: signon.actions.types.SUBMIT_SIGNON, func: submitSignon },
   { action: signon.actions.types.SIGNOUT, func: signout },
   { action: signon.actions.types.RECOVER_PASSWORD, func: recoverPassword },
-  { action: signon.actions.types.CONNECT_VIA_FACEBOOK, func: connectViaFacebook }
+  {
+    action: signon.actions.types.CONNECT_VIA_FACEBOOK,
+    func: connectViaFacebook
+  }
 ];
